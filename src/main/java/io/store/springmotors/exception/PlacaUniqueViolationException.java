ce9549b86f0343d28739c857b45b46fa485bf8c9
cleitonsilva14 <cleiton18ads@gmail.com>
2025-02-22 12:07:04 -0300

    Criação de classe Exception de Placa única

---
+++ src/main/java/io/store/springmotors/exception/PlacaUniqueViolationException.java
@@ -0,0 +1,7 @@
+package io.store.springmotors.exception;
+
+public class PlacaUniqueViolationException extends RuntimeException {
+    public PlacaUniqueViolationException(String message) {
+        super(message);
+    }
+}
