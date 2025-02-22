b51d108c2f023be05aee20b6f9ca6b90672cbd06
cleitonsilva14 <cleiton18ads@gmail.com>
2025-02-22 09:08:23 -0300

    Gerando documentação GetAll CarroController

--- src/main/java/io/store/springmotors/controller/CarroController.java
+++ src/main/java/io/store/springmotors/controller/CarroController.java
@@ -5,6 +5,11 @@ import io.store.springmotors.dto.CarroResponseDto;
 import io.store.springmotors.mapper.CarroMapper;
 import io.store.springmotors.model.CarroModel;
 import io.store.springmotors.service.CarroService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 import lombok.RequiredArgsConstructor;
@@ -20,6 +25,7 @@ import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Tag(name = "Carros", description = "Operações relacionadas aos carros")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/carros")
@@ -28,6 +34,19 @@ public class CarroController {
     private final CarroService carroService;
 
 
+    @Operation(
+            summary = "Criar um novo carro",
+            description = "Recurso responsável por obter todos os carros cadastrados",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200", description = "Executado com sucesso",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = CarroResponseDto.class)
+                            )
+                    )
+            }
+    )
     @GetMapping
     public ResponseEntity<List<CarroResponseDto>> getAll(){
         List<CarroModel> carros = carroService.findAllCarros();
@@ -46,7 +65,7 @@ public class CarroController {
 
     }
 
-    @PostMapping("/all")
+    @PostMapping
     public ResponseEntity<List<CarroModel>> saveAll(@RequestBody @Valid List<CarroRequestDto> carroRequestDtoList){
 
         // return carros.stream().map(carroModel -> toDto(carroModel)).collect(Collectors.toList());
@@ -59,11 +78,16 @@ public class CarroController {
 
     }
 
+    /**
+     *
+     * Post Mapping oneCar
+     * */
+    /*
     @PostMapping
     public ResponseEntity<CarroResponseDto> save(@RequestBody @Valid CarroRequestDto carroRequestDto){
         CarroModel carro = carroService.saveCar(CarroMapper.toCarro(carroRequestDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(CarroMapper.toDto(carro));
-    }
+    }*/
 
 
 }
